6f4b952f391829ce05a7a88d5d35186046243b73
JackSparrow9875 <rishiveer07.kv@gmail.com>
2024-01-02 20:03:52 +0530

    Added stylesheet

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/about/<username>')
 def about_page(username):
     return f'<h1>About {username}</h1>'
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
