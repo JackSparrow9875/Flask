43bdbd04707bb93896456cd24b0c9a5170de2ea2
JackSparrow9875 <rishiveer07@outlook.com>
2024-02-03 14:58:46 +0530

    major changes made and errors minimized

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ""
+app.config['SECRET_KEY'] = "secret_key@123"
+
+class NamerForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
@@ -14,6 +18,16 @@ def index():
 def user(name):
     return render_template('user.html',name=name)
 
+@app.route('/form', methods=["GET", "POST"])
+def exform():
+    name = None
+    form = NamerForm()
+    #Validating the form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    
+    return render_template('name.html', name=name, form=form)
 
 #ERROR HANDLING
 @app.errorhandler(404)
