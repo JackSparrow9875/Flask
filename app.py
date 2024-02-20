c2b4625e447476c3face1619d7ebb5c1a9d75588
JackSparrow9875 <rishiveer07@outlook.com>
2024-02-21 03:22:55 +0530

    Finally made an operational sqlite database and started with the user_update form

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import sqlite3
-
+from datetime import datetime
 
 app = Flask(__name__)
 
 #secret key
 app.config['SECRET_KEY'] = "secret_key@123"
 
-con = sqlite3.connect('NovaCart.db')
-cur = con.cursor()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///NovaCart.db'
+
+
 
 class UserForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
@@ -19,6 +20,12 @@ class UserForm(FlaskForm):
     password1 = PasswordField("Enter your password", validators=[DataRequired()])
     password2 = PasswordField("Re-enter your password", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+def get_cursor():
+    con = sqlite3.connect('NovaCart.db')
+    cur = con.cursor()
+    return con, cur
 
 
 @app.route('/')
@@ -40,16 +47,63 @@ def add_user():
             name = form.name.data
             email = form.email.data
             password = form.password1.data
-            cur.execute('''INSERT INTO Users(Name, Email, Password) VALUES (?,?,?)''', (name,email,password))
-            con.commit()
-            con.close()
-            flash('User added successfully!')
-            return redirect(url_for('user', name=name))
+            try:
+                con, cur = get_cursor()
+                cur.execute('''INSERT INTO Users(Name, Email, Password) VALUES (?,?,?)''', (name,email,password))
+                con.commit()
+                flash('User added successfully!')
+                return redirect(url_for('user', name=name))
+            except sqlite3.Error as e:
+                flash(f'An error occured: {str(e)}')
+            finally:
+                if con:
+                    con.close()
         else:
             flash("Passwords donot match, please try again")
     
     return render_template('usersignup.html', form=form)
 
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    con, cur = get_cursor()
+    cur.execute("SELECT * FROM Users WHERE id=?", (id,))
+    user = cur.fetchone()
+    if user is None:
+        flash('Error! User does not exist')
+    
+    if request.method == 'POST' and form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        try:
+            cur.execute('''UPDATE Users SET Name=?, Email=? WHERE id=?''', (name,email,id))
+            con.commit()
+            flash('User information updated successfully!')
+            return render_template('update_user.html', form=form)
+        except sqlite3.Error as e:
+            flash(f'An error occurred: {str(e)}')
+        finally:
+            if con:
+                con.close()
+    elif request.method == 'GET':
+        form.name.data = user[1]
+        form.email.data = user[2]
+        form.password1.data = user[3]
+    return render_template('update_user.html', form=form, id=id)
+
+@app.route('/user_list')
+def userlist():
+    try:
+        con, cur = get_cursor()
+        cur.execute('''SELECT * FROM Users''')
+        users = cur.fetchall()
+        return render_template('userlist.html', users=users)
+    except Exception as e:
+        flash(f'An error occured: {str(e)}')
+    finally:
+        if con:
+            con.close()
 
 #ERROR HANDLING
 @app.errorhandler(404)
