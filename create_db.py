15afbce34f22412b7ffcff8ede7118aa6b1e28a1
JackSparrow9875 <rishiveer07@outlook.com>
2024-03-04 10:00:58 +0530

    major changes made to the databases

--- create_db.py
+++ create_db.py
@@ -1,46 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 
+con = sqlite3.connect('NovaCart.db')
+con.close()
 
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NovaCart.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NovaCart.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100), unique=True)
-    fav_color = db.Column(db.String(50))
-    hashed_password = db.Column(db.String(128))
-
-    def __repr__(self):
-        return f"<User {self.name}>"
-
-    @property
-    def password(self):
-        raise AttributeError('Password is not a readable attribute!')
-
-    @password.setter
-    def password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.hashed_password, password)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+print('NovaCart database has been successfully created')
